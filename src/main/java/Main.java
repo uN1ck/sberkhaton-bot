74245fa20830973f0dca69823cc49b973b4f68b8
Siberian Barbarian <qserxsdiv@gmail.com>
2019-10-14 10:03:29 +0700

    Добавил темплейт из доки

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.ExecutionException;
+import im.dlg.botsdk.Bot;
+import im.dlg.botsdk.BotConfig;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        BotConfig botConfig = BotConfig.Builder.aBotConfig()
+                .withHost("grpc-test.transmit.im")
+                .withPort(9443)
+                .withToken("e60137c00345e62ea8a21506cfe31b2be10852ec").build();
+
+        bot.messaging().onMessage(message ->
+                bot.users().get(message.getSender()).thenAccept(userOpt -> userOpt.ifPresent(user -> {
+                            System.out.println("Got a message: " + message.getText() + " from user: " + user.getName());
+                        })
+                ).thenCompose(aVoid -> {
+                            bot.messaging().sendText(message.getPeer(), "Reply to : " + message.getMessageContent().toString());
+                            return null;
+                        }
+                ).thenAccept(uuid ->
+                        System.out.println("Sent a message with UUID: " + uuid)));
+
+        bot.await();
+    }
+}
