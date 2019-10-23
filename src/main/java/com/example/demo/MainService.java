7f6e1909eaad60cd3ae7ab8812b8198d2e7fe353
Siberian Barbarian <qserxsdiv@gmail.com>
2019-10-23 12:05:54 +0700

    Вытаскиваю самого бота отдельно

--- src/main/java/com/example/demo/MainService.java
+++ src/main/java/com/example/demo/MainService.java
@@ -4,30 +4,26 @@ import im.dlg.botsdk.Bot;
 import im.dlg.botsdk.BotConfig;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.ExecutionException;
 
 @Service
 public class MainService {
-    public void main(String[] args) throws InterruptedException, ExecutionException {
-
+    @PostConstruct
+    public void main() throws InterruptedException, ExecutionException {
         BotConfig botConfig = BotConfig.Builder.aBotConfig()
-                .withHost("grpc-test.transmit.im")
-                .withPort(9443)
-                .withToken("").build();
+                                               .withHost("hackathon-mob.transmit.im")
+                                               .withPort(443)
+                                               .withToken("").build();
 
         Bot bot = Bot.start(botConfig).get();
 
-        bot.messaging().onMessage(message ->
-                bot.users().get(message.getSender()).thenAccept(userOpt -> userOpt.ifPresent(user -> {
-                            System.out.println("Got a message: " + message.getText() + " from user: " + user.getName());
-                        })
-                ).thenCompose(aVoid -> {
-                            bot.messaging().sendText(message.getPeer(), "Reply to : " + message.getMessageContent().toString());
-                            return null;
-                        }
-                ).thenAccept(uuid ->
-                        System.out.println("Sent a message with UUID: " + uuid)));
+        bot.messaging().onMessage(message -> {
+            bot.messaging().sendText(message.getPeer(), "REPLIED! " + message.getText());
+        });
 
-        bot.await();
+        // bot.await();
     }
+
+
 }
